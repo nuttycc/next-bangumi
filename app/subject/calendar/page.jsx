1cc5c594ebdab4b15ff302afd0054fdad1105b93
nuttycc <73019368+nuttycc@users.noreply.github.com>
2023-11-19 20:32:34 +0800

    fix

--- app/subject/calendar/page.jsx
+++ app/subject/calendar/page.jsx
@@ -1,6 +1,6 @@
 import { getCalendar } from "../../lib/subject";
 import AniCalender from "../../ui/subject/calendar";
-import AniCalenderOnMd from "@/app/ui/subject/calendarM";
+import AniCalenderOnMd from "../../ui/subject/calendarM";
 
 export default async function Page() {
   const data = await getCalendar();
