1acd797110b1a2685fbdbb91611658f195e69848
nuttycc <73019368+nuttycc@users.noreply.github.com>
2023-11-22 18:17:06 +0800

    fix

--- app/character/page.jsx
+++ app/character/page.jsx
@@ -1,11 +1,7 @@
-import {getDetails, getRelatedSubjects ,getRelatedPersons} from '@/app/lib/character'
 
-export default async function Character({ }) {
-  const [imageData, detailsData, personsData, subjectsData] = await Promise.all(
-    [
-      getDetails(characterId),
-      getRelatedPersons(characterId),
-      getRelatedSubjects(characterId),
-    ],
-  );
+
+export default function Character() {
+  return (
+    <div>page</div>
+  )
 }
