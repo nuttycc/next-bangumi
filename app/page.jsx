9c9b60e8e188b8238f87376c8d6714fb2323c4f2
nuttycc <73019368+nuttycc@users.noreply.github.com>
2023-11-24 12:53:22 +0800

    change home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,52 @@
-export default function Page() {
+import { getCalendar } from "./lib/subject";
+import Image from "next/image";
+import Scroll from "./ui/scroll";
+
+const date = new Date()
+const today = date.getDay() === 0 ? 7 : date.getDay() // 0 1 2 3...
+export default async function Page() {
+  const calendar = await getCalendar()
+  const todayList = calendar[today - 1].items.map((x) => {
+    return (
+      <div key={x.id}>
+        <div className="relative h-[150px] w-[30vw]">
+          <Image
+            src={x.images.common}
+            alt={x.name}
+            fill
+            sizes="33vw"
+            className="object-cover object-top border"
+          />
+        </div>
+        <a href={`/subject/${x.id}`} className="block w-[30vw] truncate">{x.name_cn || x.name}</a>
+
+        {/* <div>{x.collection.doing}</div> */}
+        {/* <div>{x.rating.score}</div> */}
+        {/* <div>{x.rating.total}</div> */}
+        <div>{}</div>
+        <div>{}</div>
+        <div>{}</div>
+      </div>
+    );
+  })
+
   return (
     <div>
       <p>Home Page</p>
+      <div className="relative">
+        今日放送
+        <div
+          className="flex h-max overflow-x-auto scroll-smooth"
+          style={{ scrollbarWidth: "1rem", scrollbarColor: "#ea580c" }}
+        >
+          {todayList}
+          <Scroll />
+        </div>
+        <div className="z-50"></div>
+      </div>
+      <div>随机番剧</div>
+      <div>今日放送</div>
+      <div>今日放送</div>
     </div>
   );
 }
---
+++ app/ui/scroll.jsx
@@ -0,0 +1,63 @@
+'use client'
+import { useEffect, useRef, useState } from "react";
+
+export default function Scroll() {
+  const arrow = useRef(null)
+  const [showLeftArrow, setShowLeftArrow] = useState(false);
+  const [showRightArrow, setShowRightArrow] = useState(false);
+  useEffect(() => {
+    const container = arrow.current.parentElement
+
+    const handleScroll = () => {
+      // 当滚动到最左边时隐藏左箭头
+      setShowLeftArrow(container.scrollLeft > 0);
+
+      // 当滚动到最右边时隐藏右箭头
+      setShowRightArrow(
+        container.scrollLeft <
+          container.scrollWidth - container.clientWidth,
+      );
+    };
+
+    container.addEventListener("scroll", handleScroll);
+
+    // 初始设置一次以检查箭头的显示状态
+    handleScroll();
+
+    return () => {
+      container.removeEventListener("scroll", handleScroll);
+    };
+  })
+
+  return (
+    <div ref={arrow} className="">
+      {showLeftArrow && (
+        <div
+          className="absolute left-0 top-1/2 h-16 w-6 -translate-y-1/2 rounded-lg border border-transparent bg-gray-800 text-white opacity-80"
+          onClick={() => {
+            const container = arrow.current.parentElement;
+            container.scrollLeft -= 250; // 向左滚动一定距离，可以根据需要自定义
+          }}
+        >
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
+            &laquo;
+          </span>
+        </div>
+      )}
+
+      {showRightArrow && (
+        <div
+          className="absolute right-0 top-1/2  h-16 w-6 -translate-y-1/2 rounded-lg border border-transparent bg-gray-800 text-white opacity-80"
+          onClick={() => {
+            const container = arrow.current.parentElement;
+            container.scrollLeft += 250; // 向右滚动一定距离，可以根据需要自定义
+          }}
+        >
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            &raquo;
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
