6f09cbc70c1be9800c9594177eaa8207a6aa41d3
nuttycc <73019368+nuttycc@users.noreply.github.com>
2024-03-01 17:04:16 +0800

    test

--- app/page.jsx
+++ app/page.jsx
@@ -10,37 +10,31 @@ export const metadata = {
 };
 
 export default async function Page() {
-  try {
-    const random = Array(10)
-      .fill(0)
-      .map((x, i) => Math.floor(200000 + i * 1000));
+  const random = Array(10)
+    .fill(0)
+    .map((x, i) => Math.floor(200000 + i * 1000));
 
-    const subjectPromises = await Promise.allSettled(
-      random.map((x) => {
-        return getSubject(x);
-      }),
-    );
+  const subjectPromises = await Promise.allSettled(
+    random.map((x) => {
+      return getSubject(x);
+    }),
+  );
 
-    const calendar = await getCalendar() || [];
+  const calendar = await getCalendar() || [];
 
-    return (
-      <div className="md:w-[66vw]">
-        <h1 className="sr-only">Home Page</h1>
-        <div className="relative mb-2 md:w-max">
-          <h2 className="text-lg">今日放送</h2>
-          <div className="flex h-max overflow-hidden scroll-smooth md:w-[66vw]">
-            <TodayAni calendar={calendar} />
-            <ScrollButton />
-          </div>
-        </div>
-        <div className="">
-          <RandomSubjects subjectPromises={subjectPromises} />
+  return (
+    <div className="md:w-[66vw]">
+      <h1 className="sr-only">Home Page</h1>
+      <div className="relative mb-2 md:w-max">
+        <h2 className="text-lg">今日放送</h2>
+        <div className="flex h-max overflow-hidden scroll-smooth md:w-[66vw]">
+          {/* <TodayAni calendar={calendar} /> */}
+          <ScrollButton />
         </div>
       </div>
-    );
-  } catch (error) {
-    console.error(`Error fetching data: ${error}`);
-    // 在这里你可以选择如何处理错误，例如返回一个错误页面
-    return <div>Something went wrong</div>;
-  }
+      <div>
+        {/* <RandomSubjects subjectPromises={subjectPromises} /> */}
+      </div>
+    </div>
+  );
 }
